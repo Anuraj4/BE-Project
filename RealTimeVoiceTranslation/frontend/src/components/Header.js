89b9d59d699e8464d6b8a80dd2379d66aacdd057
Saideep <saideepchougale2115@gmail.com>
2024-09-19 11:54:53 +0530

    changing header

--- RealTimeVoiceTranslation/frontend/src/components/Header.js
+++ RealTimeVoiceTranslation/frontend/src/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Header = () => {
   return (
     <header>
-      <h1>Real-Time Voice Translation</h1>
+      <h1>Real-Time AI Voice Translation</h1>
     </header>
   );
 };
