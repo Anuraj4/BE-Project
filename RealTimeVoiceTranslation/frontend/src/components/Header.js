0b687b320e49fbee827cd50623ee6623a226a98b
Anuraj Venkatpurwar <anurajvenkatpurwar@gmail.com>
2024-10-21 11:55:11 +0530

    Changing the font weight

--- RealTimeVoiceTranslation/frontend/src/components/Header.js
+++ RealTimeVoiceTranslation/frontend/src/components/Header.js
@@ -4,7 +4,7 @@ const Header = () => {
   return (
     <header className='gradient-bg py-3'>
       <div className='container text-center'>
-        <h1 className='text-black display-4'>Real-Time Voice Translation</h1>
+        <h1 className='text-black custom-bold'>Real-Time Voice Translation</h1>
       </div>
     </header>
   );
